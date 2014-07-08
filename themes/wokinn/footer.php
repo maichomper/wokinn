f8a60d92572d28c0cbe93554938fae23483ad0f9
maichomper <maichomper@gmail.com>
2014-07-08 13:46:27 -0500

    conflictos

--- themes/wokinn/footer.php
+++ themes/wokinn/footer.php
@@ -4,12 +4,9 @@
 		</div><!-- container -->
 	</body>
 
-<<<<<<< HEAD
-=======
 	<script src="js/jquery.js"></script>
 	<script src="js/plugins.js"></script>
 	<script src="js/functions.js"></script>
 	<script src="https://maps.googleapis.com/maps/api/js?v=3.exp"></script>
 
->>>>>>> b9a630b74f66d7f6ee2d4163baf3726dfad41486
 </html>
