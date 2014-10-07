0faf5c54c6b8e45255db4505ea670054c8049d0d
raul <raul@zamacona.me>
2014-10-07 17:22:40 -0500

    stuff

--- themes/wokinn/inc/pages.php
+++ themes/wokinn/inc/pages.php
@@ -50,5 +50,16 @@
 			wp_insert_post( $page, true );
 		}
 
+		// Info nosotros
+		if( ! get_page_by_path('info-nosotros') ){
+			$page = array(
+				'post_author' => 1,
+				'post_status' => 'publish',
+				'post_title'  => 'Info nosotros',
+				'post_type'   => 'page'
+			);
+			wp_insert_post( $page, true );
+		}
+
 
 	});
